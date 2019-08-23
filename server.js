49b5d20396e34da5c573f37dfdc03436c13d4c43
JefferySJones <jeffery.jones@lendio.com>
2019-08-22 20:03:16 -0600

    rearrange and clean up

--- server.js
+++ server.js
@@ -1,5 +1,4 @@
 // server.js
-
 const express = require('express');
 const app = express();
 const fetch = require('node-fetch');
@@ -8,9 +7,11 @@ const get = require('lodash/get');
 const cloneDeep = require('lodash/cloneDeep');
 const { Client } = require('pg');
 
+// Set up environment variables
 const dotenv = require('dotenv');
 dotenv.config();
 
+// Set up postgres client
 const connectionString = process.env.DATABASE_URL;
 const pgclient = new Client({
     connectionString,
@@ -18,39 +19,27 @@ const pgclient = new Client({
 });
 pgclient.connect();
 
+app.listen(process.env.PORT || 4000, function(){
+    console.log('Your node js server is running');
+});
+
 // Parse JSON bodies
 app.use(express.json());
 
 // Parse url-encoded bodies
 app.use(express.urlencoded({extended: true}));
 
+/**
+ * GET Routes
+ */
+
 app.get('/', function(req, res) {
     res.send('Healthy');
 });
 
-function modifyMessage (newMessage, optsToRemove = [], appendage) {
-    let attachment = get(newMessage, 'attachments[0]');
-    attachment.text = attachment.text + appendage;
-
-    newMessage.attachments = [];
-
-    // Remove used option
-    let options = get(attachment, 'actions[0].options');
-    options = options.reduce((accumulator, option) => {
-        if (!optsToRemove.includes(option.value)) {
-            accumulator.push(option);
-        }
-        return accumulator;
-    }, []);
-    attachment.actions[0].options = options;
-
-    newMessage.attachments.push(
-        attachment
-    );
-
-    return newMessage;
-}
-
+/**
+ * POST Routes
+ */
 app.post('/action/', async function(req, res) {
     const payload = get(req, 'body.payload');
 
@@ -86,6 +75,110 @@ app.post('/action/', async function(req, res) {
         res.sendStatus(403);
     }
 });
+
+app.post('/pullrequest/', async function(req, res) {
+    const body = get(req, 'body');
+    const action = get(body, 'action');
+
+    const branch = get(body, 'pull_request.head.ref');
+    const pull_request = get(body, 'pull_request');
+
+    if (process.env.DEBUG === 'true' && branch) {
+        console.log('branch ', branch);
+    }
+
+    if (action !== 'labeled') {
+        // res.send('Action is not labeled or there is no body');
+        // return;
+    }
+
+    if (action == 'labeled') {
+        processLabeled(body, res);
+    }
+
+    if (action == 'review_requested') {
+        const reviewers = get(body, 'pull_request.requested_reviewers').map(function (reviewer) {
+            return reviewer && reviewer.login
+        });
+        
+        const message = {
+            "parse": "full",
+            "text": pull_request.user.login + ' is requesting a review from:' + '\n' +
+                '    ' + reviewers.join(',') + '\n' +
+                'On branch: `' + branch + '`',
+            "response_type": "in_channel",
+            "attachments": [
+                {
+                    "fallback": pull_request.html_url,
+                    "title": pull_request.title,
+                    "text": pull_request.html_url,
+                    "color": '#' + '2BABE2',
+                    "attachment_type": "default"
+                }
+            ]
+        };
+        
+        const params = {
+            text: message.text,
+            attachments: JSON.stringify(message.attachments),
+            parse: message.parse,
+            response_type: message.response_type,
+            token: process.env.BOT_TOKEN,
+            channel: process.env.CHANNEL
+        };
+
+        const query =  Object.keys(params)
+            .map(k => encodeURIComponent(k) + '=' + encodeURIComponent(params[k]))
+            .join('&');
+
+        const slack_message = await fetch('https://slack.com/api/chat.postMessage?' + query, {
+            method: 'POST'
+        }).then((res) => res.json())
+        const timestamp = get(slack_message, 'ts');
+        
+        savePullsMessages(branch, timestamp);
+
+        res.send({ branch, timestamp });
+        return;
+    }
+});
+
+/**
+ * Methods
+ */
+function modifyMessage (newMessage, optsToRemove = [], appendage) {
+    let attachment = get(newMessage, 'attachments[0]');
+    attachment.text = attachment.text + appendage;
+
+    newMessage.attachments = [];
+
+    // Remove used option
+    let options = get(attachment, 'actions[0].options');
+    options = options.reduce((accumulator, option) => {
+        if (!optsToRemove.includes(option.value)) {
+            accumulator.push(option);
+        }
+        return accumulator;
+    }, []);
+    attachment.actions[0].options = options;
+
+    newMessage.attachments.push(
+        attachment
+    );
+
+    return newMessage;
+}
+
+function savePullsMessages (timestamp, branch) {
+    const text = 'INSERT INTO pulls_messages(message_ts, branch) VALUES($1, $2) RETURNING *';
+    const values = [timestamp, branch];
+    
+    return pgclient.query(text, values)
+        .then(res => {
+            console.log(res.rows[0])
+        })
+        .catch(e => console.error(e.stack));
+}
 
 async function processLabeled (body, res) {
     const pull_request = get(body, 'pull_request');
@@ -162,82 +255,3 @@ async function processLabeled (body, res) {
         return;
     }
 }
-
-app.post('/pullrequest/', async function(req, res) {
-    const body = get(req, 'body');
-    const action = get(body, 'action');
-
-    const branch = get(body, 'pull_request.head.ref');
-    const pull_request = get(body, 'pull_request');
-
-    if (process.env.DEBUG === 'true' && branch) {
-        console.log('branch ', branch);
-    }
-
-    if (action !== 'labeled') {
-        // res.send('Action is not labeled or there is no body');
-        // return;
-    }
-
-    if (action == 'labeled') {
-        processLabeled(body, res);
-    }
-
-    if (action == 'review_requested') {
-        const reviewers = get(body, 'pull_request.requested_reviewers').map(function (reviewer) {
-            return reviewer.login
-        });
-        
-        const message = {
-            "parse": "full",
-            "text": pull_request.user.login + ' is requesting a review from:' + '\n' +
-                '    ' + reviewers.join(',') + '\n' +
-                'On branch: `' + branch + '`',
-            "response_type": "in_channel",
-            "attachments": [
-                {
-                    "fallback": pull_request.html_url,
-                    "title": pull_request.title,
-                    "text": pull_request.html_url,
-                    "color": '#' + '2BABE2',
-                    "attachment_type": "default"
-                }
-            ]
-        };
-        
-        const params = {
-            text: message.text,
-            attachments: JSON.stringify(message.attachments),
-            parse: message.parse,
-            response_type: message.response_type,
-            token: process.env.BOT_TOKEN,
-            channel: process.env.CHANNEL
-        };
-
-        const query =  Object.keys(params)
-            .map(k => encodeURIComponent(k) + '=' + encodeURIComponent(params[k]))
-            .join('&');
-
-        const slack_message = await fetch('https://slack.com/api/chat.postMessage?' + query, {
-            method: 'POST'
-        }).then((res) => res.json())
-
-        const timestamp = get(slack_message, 'ts')
-
-        const text = 'INSERT INTO pulls_messages(message_ts, branch) VALUES($1, $2) RETURNING *';
-        const values = [timestamp, branch];
-        
-        pgclient.query(text, values)
-            .then(res => {
-                console.log(res.rows[0])
-            })
-            .catch(e => console.error(e.stack));
-
-        res.send({ branch, timestamp });
-        return;
-    }
-});
-
-app.listen(process.env.PORT || 4000, function(){
-    console.log('Your node js server is running');
-});
